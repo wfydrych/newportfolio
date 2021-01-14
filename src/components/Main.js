58cffdcf017000d356b9b757995cee41143940ca
Wojciech Fydrych <wfydrych@gmail.com>
2021-01-14 19:00:23 +0100

    React router animation

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,27 +4,35 @@ import Technologies from './Technologies'
 import Projects from './Projects'
 import React from 'react'
 import {Route, Switch} from 'react-router-dom'
+import {AnimatedSwitch} from 'react-router-transition'
 
 export default function Main() {
   return (
     <div className='main'>
         <Switch>
-            <Route 
-                exact path='/' 
-                render = {text => <About></About>}>
-            </Route>
-            <Route 
-                path='/about' 
-                render = {text => <About></About>}>
-            </Route>
-            <Route 
-                path='/technologies' 
-                render = {text => <Technologies></Technologies>}>
-            </Route>
-            <Route 
-                path='/projects' 
-                render = {text => <Projects></Projects>}>
-            </Route>
+            <AnimatedSwitch
+                atEnter={{ opacity: 0 }}
+                atLeave={{ opacity: 0 }}
+                atActive={{ opacity: 1 }}
+                className="switch-wrapper"
+            >
+                <Route 
+                    exact path='/' 
+                    render = {text => <About></About>}>
+                </Route>
+                <Route 
+                    path='/about' 
+                    render = {text => <About></About>}>
+                </Route>
+                <Route 
+                    path='/technologies' 
+                    render = {text => <Technologies></Technologies>}>
+                </Route>
+                <Route 
+                    path='/projects' 
+                    render = {text => <Projects></Projects>}>
+                </Route>
+            </AnimatedSwitch>
         </Switch>
     </div>
   );
