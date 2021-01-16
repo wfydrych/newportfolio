6d26295763eae83e165069105936f2933bd24585
Wojciech Fydrych <wfydrych@gmail.com>
2021-01-16 22:36:58 +0100

    Projects added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,8 @@ import weather from './img/projects/weather.png'
 import habit from './img/projects/habit.png'
 import todo from './img/projects/todo.png'
 import krzys from './img/projects/krzys.png'
+import pavel from './img/projects/pavelmiguel.png'
+import carshine from './img/projects/carshine.png'
 
 export default function Projects() {
   return (
@@ -10,31 +12,44 @@ export default function Projects() {
         <div className='projects__title'>My projects</div>
         <div className='projects__container'>
           <div className='projects__container__project'>
-            <img alt='portfolio-kristoff' src={krzys} className='projects__container__project__pic' />
+            <a target="_blank" href="https://wfydrych.github.io/carshine/"><img alt='car-shine-factory' src={carshine} className='projects__container__project__pic' /></a>
+            <div className='projects__container__project__description'>
+              <span>Car Shine Factory</span>
+                (in progress)
+            </div>
+          </div>
+          <div className='projects__container__project'>
+            <a target="_blank" href="https://wfydrych.github.io/pavelmiguel/"><img alt='portfolio-pavel-miguel' src={pavel} className='projects__container__project__pic' /></a>
+            <div className='projects__container__project__description'>
+              <span>Portfolio Pavel Miguel</span>
+                (in progress)
+            </div>
+          </div>
+          <div className='projects__container__project'>
+            <a target="_blank" href="https://guarded-fortress-34922.herokuapp.com/"><img alt='portfolio-kristoff' src={krzys} className='projects__container__project__pic' /></a>
             <div className='projects__container__project__description'>
               <span>Portfolio</span>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id eros commodo, sollicitudin tortor nec, 
+                (in progress)
             </div>
           </div>
           <div className='projects__container__project'>
             <img alt='HabitTracker' src={habit} className='projects__container__project__pic' />
             <div className='projects__container__project__description'>
               <span>Habit Tracker</span>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id eros commodo, sollicitudin tortor nec, 
+                (in progress)
             </div>
           </div>
           <div className='projects__container__project'>
-            <img alt='ToDoApp' src={todo} className='projects__container__project__pic' />
+            <a target="_blank" href="https://to-do-list-mobile.herokuapp.com/"><img alt='ToDoApp' src={todo} className='projects__container__project__pic' /></a>
             <div className='projects__container__project__description'>
               <span>To Do App</span>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id eros commodo, sollicitudin tortor nec, 
             </div>
           </div>
           <div className='projects__container__project'>
-            <img alt='weather-app' src={weather} className='projects__container__project__pic' />
+            <a target="_blank" href="https://wfydrych.github.io/WeatherApp/"><img alt='weather-app' src={weather} className='projects__container__project__pic' /></a>
             <div className='projects__container__project__description'>
               <span>Weather App</span>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id eros commodo, sollicitudin tortor nec, 
+              
             </div>
           </div>
         </div>
